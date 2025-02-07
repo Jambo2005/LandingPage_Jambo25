a37a3337420e47c99cdf39f5a61a59c2455cef9b
Jambo2005 <meathsjambo7252005@gmail.com>
2025-02-07 12:46:53 +0800

    Add files via upload

---
+++ Jambo/jambo/BAD.py
@@ -0,0 +1,12 @@
+# import counter
+c = ([1,1,2,3,2,2,4,5,4,3,6,2,2])
+print(c)
+
+ # import deque
+d = ([1,2,3])
+print(d)
+
+# import namedtuple
+Point = ('Point', ['x','y'])
+p = Point(10,20)
+print(p.x, p.y)
---
+++ Jambo/jambo/badilla.py
@@ -0,0 +1,9 @@
+students = {"BADILLA","BAROT","URBANO"}
+print(students)
+students.add("IGOY")
+print(students.add)
+students.update(["RIOLA","JUMAMIL"])
+print(students.update)
+len(students)
+print(len(students))
+print(students)
